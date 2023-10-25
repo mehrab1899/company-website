6f5c5f6a155cc2b5fa46ec7521229a931e814544
mehrab1899 <mehrabmehmood1899@gmail.com>
2023-10-25 12:44:15 +0500

    layout defined

---
+++ src/layout/Quotation.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Quotation = () => {
+  return <div>Quotation</div>;
+};
+
+export default Quotation;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Hero from "../layout/Hero";
+import WhyUs from "../layout/WhyUs";
+import Services from "../layout/Services";
+import Technology from "../layout/Technology";
+import Portfolio from "../layout/Portfolio";
+import Footer from "../components/Footer";
 
 const Home = () => {
-  return <div>Company Homepage</div>;
+  return (
+    <div>
+      <Hero />
+      <WhyUs />
+      <Services />
+      <Technology />
+      <Portfolio />
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
